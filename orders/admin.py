2e2492f3dfc6780d826656a93caf26f1cd762a0f
Amir Karimi <amirmohamadkarimi13901390@gmail.com>
2026-08-12 23:04:48 +0430

    fix some issues

--- orders/admin.py
+++ orders/admin.py
@@ -1,10 +1,31 @@
 from django.contrib import admin
 
-from .models import Coupon
+from .models import Order, OrderItem, Coupon
+
+
+class OrderItemInlaine(admin.TabularInline):
+    model = OrderItem
+    fields = ('order', 'book', 'quantity', 'price', )
+    extra = 1
+
+
+@admin.register(Order)
+class OrderAdmin(admin.ModelAdmin):
+    list_display = ('user', 'full_name', 'created_at', 'status', )
+
+    inlines = [
+        OrderItemInlaine,
+    ]
+
+
+@admin.register(OrderItem)
+class OrderItemAdmin(admin.ModelAdmin):
+    list_display = ('order', 'book', 'quantity', 'price', )
 
 
 @admin.register(Coupon)
 class CouponAdmin(admin.ModelAdmin):
-    list_display = ('code', 'discount_percent', 'valid_from', 'valid_to', 'times_used', 'usage_limit', 'is_active')
+    list_display = ('code', 'discount_percent', 'valid_from',
+                    'valid_to', 'times_used', 'usage_limit', 'is_active')
     list_filter = ('is_active',)
     search_fields = ('code',)
